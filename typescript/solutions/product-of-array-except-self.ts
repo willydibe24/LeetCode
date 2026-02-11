78ac15fc05797b7ea25b31d6bc956b46785195f8
willydibe24 <willy.dibe24@gmail.com>
2026-02-11 16:20:23 +0100

    product of array except self

--- typescript/solutions/product-of-array-except-self.ts
+++ typescript/solutions/product-of-array-except-self.ts
@@ -1,5 +1,17 @@
 function productExceptSelf(nums: number[]): number[] {
+    const result = new Array(nums.length).fill(1);
+    
+    let prefix = 1;
     for (let i = 0; i < nums.length; i++) {
-        
+        result[i] = prefix;
+        prefix *= nums[i];
     }
+
+    let suffix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
 }
